0eb5e997c26402bc566b44042c3b11e9320426ae
CoderXYZ14 <s1404islam@gmail.com>
2024-04-27 19:09:08 +0530

    Added show route

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
+const path = require("path");
 
 const MONGO_URL = "mongodb://localhost:27017/wanderlust";
 main()
@@ -14,20 +15,29 @@ main()
 async function main() {
   await mongoose.connect(MONGO_URL);
 }
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 app.get("/", (req, res) => {
   res.send("Hi , I am root");
 });
-app.get("/testListing", async (req, res) => {
-  let sampleListing = new Listing({
-    title: "My new Villa",
-    description: "By now",
-    price: 1200,
-    location: "india",
-    country: "india",
-  });
-  await sampleListing.save();
-  res.send("Successfull");
+
+app.get("/listings", async (req, res) => {
+  const allListings = await Listing.find({});
+  res.render("listings/index", { allListings }); // Corrected path
 });
+
+// app.get("/testListing", async (req, res) => {
+//   let sampleListing = new Listing({
+//     title: "My new Villa",
+//     description: "By now",
+//     price: 1200,
+//     location: "india",
+//     country: "india",
+//   });
+//   await sampleListing.save();
+//   res.send("Successfull");
+// });
 app.listen(8080, () => {
   console.log("server is listening to port 8080");
 });
